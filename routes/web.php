f8212889b7620ca92109abe1e8bd05cf68367512
Yu Qin <zxas5820@yahoo.com.tw>
2019-11-05 17:49:28 +0800

    修改路由改成controller回傳網頁

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,22 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('index', function () {
-    return view('index');
-});
-Route::get('post', function () {
-    return view('post');
-});
-Route::get('contact', function () {
-    return view('contact');
-});
-Route::get('about', function () {
-    return view('about');
-});
+//Route::get('index', function () {
+//    return view('index');
+//});
+//Route::get('post', function () {
+//    return view('post');
+//});
+//Route::get('contact', function () {
+//    return view('contact');
+//});
+//Route::get('about', function () {
+//    return view('about');
+//});
+Route::get('posts', ['as' => 'posts.index', 'uses' =>
+    'PostsController@index']);
+Route::get('post', ['as' => 'posts.show', 'uses' => 'PostsController@show']);
+Route::get('about', ['as' => 'posts.about', 'uses' =>
+    'PostsController@about']);
+Route::get('contact', ['as' => 'posts.contact', 'uses' =>
+    'PostsController@contact']);
