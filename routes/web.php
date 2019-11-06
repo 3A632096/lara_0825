8c3710076375eb354be84012b04fd47ebf272e7e
Yu Qin <zxas5820@yahoo.com.tw>
2019-11-06 15:28:31 +0800

    練習4.3. 換用 save方法

--- routes/web.php
+++ routes/web.php
@@ -34,11 +34,16 @@ Route::get('/', function () {
 //    dd($posts);
 
 //更新資料
+//    $post=\App\Post::find(1);
+//    $post->update([
+//        'title'=>'updated title',
+//        'content'=>'updated content',
+//    ]);
+
     $post=\App\Post::find(1);
-    $post->update([
-        'title'=>'updated title',
-        'content'=>'updated content',
-    ]);
+    $post->title='saved title';
+    $post->content='saved content';
+    $post->save();
 
     return view('welcome');
 });
