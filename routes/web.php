136fb924eaf08a315e70b908f4c726d7c10f8de0
Yu Qin <zxas5820@yahoo.com.tw>
2019-12-18 02:47:25 +0800

    練習7-2, 7-3, 7-4

--- routes/web.php
+++ routes/web.php
@@ -62,9 +62,14 @@ Route::get('/', function () {
 //取得 Model
 //    $fourthPost=\App\Post::find(4);
 //    dd($fourthPost);
-    $lastPost=\App\Post::orderBy('id','DESC')->first();
-    dd($lastPost);
+//    $lastPost=\App\Post::orderBy('id','DESC')->first();
+//    dd($lastPost);
 
+    $post = \App\Post::find(6);
+    echo $post->id.'   '.$post->title.'<br><hr>';
+    foreach($post->comments as $comment) {
+        echo '*'.$comment->content.'<br>';
+    }
 
 
     return view('welcome');
